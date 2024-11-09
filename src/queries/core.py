5cb51f87dff9ac72a2c545d770a984dbd76e8d19
ConstantinePPV <constantine.ppv@gmail.com>
2024-11-09 23:28:11 +0400

    SqlAlchemy_Lesson_6

--- src/queries/core.py
+++ src/queries/core.py
@@ -1,4 +1,4 @@
-from sqlalchemy import text, insert
+from sqlalchemy import text, insert, select, update
 from database import sync_engine, async_engine
 from models import metadata_obj, workers_table
 
@@ -8,30 +8,61 @@ def get_123():
 		res = conn.execute(text("SELECT VERSION()"))
 		print(f"{res.first()=}")
 
-
 async def get_123():
 	async with async_engine.connect() as conn:
 		res = await conn.execute(text("SELECT VERSION()"))
 		print(f"{res.first()=}")
 
 
-def create_tables():
-	sync_engine.echo = False
-	metadata_obj.drop_all(sync_engine)
-	metadata_obj.create_all(sync_engine)
-	sync_engine.echo = True
+class SyncCore:
+	@staticmethod
+	def create_tables():
+		sync_engine.echo = False
+		metadata_obj.drop_all(sync_engine)
+		metadata_obj.create_all(sync_engine)
+		sync_engine.echo = True
+
+	@staticmethod
+	def insert_workers(): 
+		with sync_engine.connect() as conn:
+			# stmt = """INSERT INTO workers (username) VALUES
+			# 	('Bobr'),
+			# 	('Volk');"""
+			stmt = insert(workers_table).values(
+				[
+					{"username": "Jack"},
+					{"username": "Michael"},
+				]
+			)
+			conn.execute(stmt)
+			conn.commit()
+
+	@staticmethod
+	def select_workers(): 
+		with sync_engine.connect() as conn:
+			query = select(workers_table) # SELECT * FROM workers
+			result = conn.execute(query)
+			workers = result.all()
+			print(f"{workers=}")
+
+	@staticmethod
+	def update_worker(worker_id: int = 2, new_username: str = "Misha"): 
+		with sync_engine.connect() as conn:
+			# stmt = text("UPDATE workers SET username=:username WHERE id=:id")
+			# stmt = stmt.bindparams(username=new_username, id=worker_id)
+			stmt = (
+				update(workers_table)
+				.values(username=new_username)
+				# .where(workers_table.c.id==worker_id)
+				.filter_by(id=worker_id)
+			)
+			conn.execute(stmt)
+			conn.commit()
 
 
-def insert_data(): 
-	with sync_engine.connect() as conn:
-		# stmt = """INSERT INTO workers (username) VALUES
-		# 	('Bobr'),
-		# 	('Volk');"""
-		stmt = insert(workers_table).values(
-			[
-				{"username": "Bobr"},
-				{"username": "Volk"},
-			]
-		)
-		conn.execute(stmt)
-		conn.commit()
+class AsyncCore:
+	@staticmethod
+	async def create_tables():
+		async with async_engine.begin() as conn:
+			await conn.run_sync(metadata_obj.drop_all)
+			await conn.run_sync(metadata_obj.create_all)
--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,19 +1,53 @@
-from sqlalchemy import text, insert
+from sqlalchemy import text, insert, select
 from database import sync_engine, async_engine,session_factory, async_session_factory, Base
 from models import WorkerOrm
 
 
+class SyncORM:
+	@staticmethod
+	def create_tables():
+		sync_engine.echo = False
+		Base.metadata.drop_all(sync_engine)
+		Base.metadata.create_all(sync_engine)
+		sync_engine.echo = True
 
-def create_tables():
-	sync_engine.echo = False
-	Base.metadata.drop_all(sync_engine)
-	Base.metadata.create_all(sync_engine)
-	sync_engine.echo = True
+	@staticmethod
+	def insert_workers():	
+		with session_factory() as session:
+			worker_jack = WorkerOrm(username="Jack")
+			worker_michael = WorkerOrm(username="Michael")
+			session.add_all([worker_jack, worker_michael])
+			session.flush()
+			session.commit()
+
+	@staticmethod
+	def select_workers():
+		with session_factory() as session:
+			query = select(WorkerOrm)
+			result = session.execute(query)
+			workers = result.scalars().all()
+			print(f"{workers=}")
+
+	@staticmethod
+	def update_worker(worker_id: int = 2, new_username: str = "Misha"):
+		with session_factory() as session:
+			worker_michael = session.get(WorkerOrm, worker_id)
+			worker_michael.username = new_username
+			session.commit()
 
 
-async def insert_data():	
-	async with async_session_factory() as session:
-		worker_bobr = WorkerOrm(username="Bobr")
-		worker_volk = WorkerOrm(username="Volk")
-		session.add_all([worker_bobr, worker_volk])
-		await session.commit()
+class AsyncORM:
+	@staticmethod
+	async def create_tables():
+		async with async_engine.begin() as conn:
+			await conn.run_sync(Base.metadata.drop_all)
+			await conn.run_sync(Base.metadata.create_all)
+
+	@staticmethod
+	async def insert_workers():
+		async with async_session_factory() as session:
+			worker_jack = WorkerOrm(username="Jack")
+			worker_michael = WorkerOrm(username="Michael")
+			session.add_all([worker_jack, worker_michael])
+			await session.flush()
+			await session.commit()
